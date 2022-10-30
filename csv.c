1dc23d9b849aa90de8ce013fd270d2dc5e7803f8
Chandler Escude <chandler.escude@gmail.com>
2022-10-30 09:38:43 -0400

    Added easy mode (previous quick-mode) and refactored
    If all columns are quick, use faster processing.

--- csv.c
+++ csv.c
@@ -82,6 +82,46 @@ void process_header(FILE* f, State* s, struct csv_parser* p) {
   csv_fini(p, field_end_headermode, row_end_headermode, s);
 }
 
+/* All our fields are printed in-order (w/ no duplicates), so we don't
+   need to store off any field data! */
+void field_end_easymode(void* field, size_t len, void* data) {
+  State* s = data;
+  s->current_column++;
+
+  /* Are we even printing this column at all? */
+  if (s->current_column < veclen(s->skip_table) &&
+      s->skip_table[s->current_column].valid) {
+
+    /* Check to see if we're the first printed field... */
+    if (s->skip_table[s->current_column].offset > 0) {
+      fputc(',', stdout);
+    }
+
+    csv_fwrite(stdout, field, len);
+  }
+}
+
+void row_end_easymode(int c, void* data) {
+  State* s = data;
+  s->current_column = 0;
+  s->current_row++;
+  fputc('\n', stdout);
+  if (s->show_progress && !(s->current_row%10000)) {
+    fprintf(stderr, "\r%zu", s->current_row - (rand()%10000));
+  }
+}
+
+void process_easymode(FILE* f, State* s, struct csv_parser* p) {
+  char buf[BUF_SIZE] = {0};
+  size_t bytes_read = 0;
+  while (!feof(f)) {
+    bytes_read = fread(buf, sizeof(char), BUF_SIZE, f);
+    csv_parse(p, buf, bytes_read, field_end_easymode, row_end_easymode, s);
+  }
+  csv_fini(p, field_end_easymode, row_end_easymode, s);
+  if (s->show_progress) fprintf(stderr, "\r%zu Complete!\n", s->current_row);
+}
+
 void field_end_fullmode(void *field, size_t len, void *data) {
   State* s = (State*)data;
   s->current_column++;
@@ -209,81 +249,75 @@ int parse_columns(Field** fields_ptr, char* str) {
   return 0;
 }
 
-int main(int argc, char** argv) {
-  FILE* f = NULL;
-  State s = {0};
-  char just_header = 1;
-  s.fields = (Field*)vecnew(sizeof(Field), 0);
-
-  Arena a = {0};
-  arinit(&a);
-  s.field_mem = &a;
-
-  /* Parse arguments... */
+int parse_options(Field** fields, FILE** f, char* show_progress, int argc, char** argv) {
   int ch;
   while ((ch = getopt(argc, argv, "hf:c:p")) != -1) {
     switch (ch) {
     case 'f':
-      f = fopen(optarg, "r");
+      if (*f) {
+	return 1;
+      }
+      
+      *f = fopen(optarg, "r");
       break;
       
     case 'c':
-      just_header = 0; /* The user specified columns, so we're not in "header" mode */
-      if (parse_columns(&s.fields, optarg)) {
-	usage();
-	exit(1);
+      if (parse_columns(fields, optarg)) {
+	return 1;
       }
       break;
 
     case 'p':
-      s.show_progress = 1;
+      *show_progress = 1;
       break;
       
     case '?':
     case 'h':
-      usage();
-      exit(1);
-      break;
+      return 1;
     }
   }
 
-  if (f == NULL) {
-    f = stdin;
+  if (*f == NULL) {
+    *f = stdin;
   }
 
+  return 0;
+}
+
+void analyze_fields(State* s, char* easy_mode) {
   /* Go through our fields and mark duplicates */
-  size_t num_fields = veclen(s.fields);
+  size_t num_fields = veclen(s->fields);
   for (size_t i=0; i<num_fields; i++) {
     for (size_t j=i+1; j<num_fields; j++) {
-      if (s.fields[i].column == s.fields[j].column) {
-        s.fields[i].skip = j-i; /* i + skip => duplicate column */
+      if (s->fields[i].column == s->fields[j].column) {
+        s->fields[i].skip = j-i; /* i + skip => duplicate column */
         break;
       }
     }
   }
 
   /* Go through our fields and find the first offset for each column */
-  s.skip_table = vecnew(sizeof(SkipLookup), 0);
+  s->skip_table = vecnew(sizeof(SkipLookup), 0);
   for (size_t i=0; i<num_fields; i++) {
-    size_t col = s.fields[i].column;
+    size_t col = s->fields[i].column;
 
     /* Make sure we have enough size_t's for the skip table */
-    if (veclen(s.skip_table) <= col) {
-      s.skip_table = vecpush(s.skip_table, 1 + col - veclen(s.skip_table));
+    if (veclen(s->skip_table) <= col) {
+      s->skip_table = vecpush(s->skip_table, 1 + col - veclen(s->skip_table));
     }
 
-    if (s.skip_table == NULL) {
+    if (s->skip_table == NULL) {
       fprintf(stderr, "Allocation failure\n");
       exit(99);
     }
 
-    if (s.skip_table[col].valid) {
+    if (s->skip_table[col].valid) {
       /* We've already recorded the first field index for this column */
       continue;
     }
     
-    s.skip_table[col].valid = 1;
-    s.skip_table[col].offset = i;
+    s->skip_table[col].valid = 1;
+    s->skip_table[col].offset = i;
   }
 
   /*
@@ -304,25 +338,46 @@ int main(int argc, char** argv) {
   for (size_t i=0; i<num_fields; i++) {
 
     /* Are we out of the initial run-up? */
-    if (s.fields[i].column <= in_order_max) {
+    if (s->fields[i].column <= in_order_max) {
       num_quick_fields = i;
       break;
     }
-    in_order_max = s.fields[i].column;
+    in_order_max = s->fields[i].column;
   }
 
   /* Mark all the "quick" fields */
   for (size_t i=0; i<num_quick_fields; i++) {
-    if (s.fields[i].column <= in_order_max) {
-      s.fields[i].quick = 1;
+    if (s->fields[i].column <= in_order_max) {
+      s->fields[i].quick = 1;
     } else {
       break;
     }
   }
 
+  *easy_mode = (num_quick_fields == num_fields);
+  
   /* for (size_t i=0; i<num_fields; i++) { */
-  /*   fprintf(stdout, "idx=%zu, col=%zu, quick=%d\n", i, s.fields[i].column, s.fields[i].quick); */
+  /*   fprintf(stdout, "idx=%zu, col=%zu, quick=%d\n", i, s->fields[i].column, s->fields[i].quick); */
   /* } */
+}
+
+int main(int argc, char** argv) {
+  FILE* f = NULL;
+  State s = {0};
+  s.fields = (Field*)vecnew(sizeof(Field), 0);
+
+  Arena a = {0};
+  arinit(&a);
+  s.field_mem = &a;
+
+  /* Parse arguments... */
+  if (parse_options(&s.fields, &f, &s.show_progress, argc, argv)) {
+    usage();
+    exit(1);
+  }
+
+  char easy_mode = 0;
+  analyze_fields(&s, &easy_mode);
 
   struct csv_parser p = {0};
   if (csv_init(&p, 0)) {
@@ -330,10 +385,15 @@ int main(int argc, char** argv) {
     exit(1);
   }
 
-  if (just_header) {
+  if (veclen(s.fields) == 0) {
     process_header(f, &s, &p);
+    /* printf("Mode: Headers\n"); */
+  } else if (easy_mode) {
+    process_easymode(f, &s, &p);
+    /* printf("Mode: Easy\n"); */
   } else {
     process_fullmode(f, &s, &p);
+    /* printf("Mode: Full\n"); */
   }
 
   csv_free(&p);
